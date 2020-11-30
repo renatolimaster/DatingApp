6defb55ab900e12a86c96f263594554c30826e8e
Renato Lima <renatolimaster@gmail.com>
2020-11-30 13:08:52 -0300

    linux

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,4 +20,4 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
